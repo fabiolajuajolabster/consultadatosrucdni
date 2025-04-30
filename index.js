2c56e4b814a9a0d41cbf8716e81011899163af0c
fabiolajuajolabster <fabiolajuajolabster@gmail.com>
2025-04-30 12:38:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 
 const app = express();
-app.set("port", 9101);
+app.set("port", process.env.PORT || 9101);
 app.use(express.json());
 
 // Añadir middleware para CORS
